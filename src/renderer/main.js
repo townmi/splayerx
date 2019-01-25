367964e697ed7713db39cd684b86bb6962eb344e
SHEN SHENG <tomasen@gmail.com>
2019-01-25 13:54:05 +0800

    Revert "fix(ga): google analytics failed to track production version"
    This reverts commit 58614dd38d50faaebacef6f24f818374b59a7e8b.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -80,7 +80,6 @@ Vue.use(VueElectronJSONStorage);
 Vue.use(VueResource);
 Vue.use(AsyncComputed);
 
-console.log('SPlayerClientVersion', electron.remote.app.getVersion());
 Vue.use(VueAnalytics, {
   id: (process.env.NODE_ENV === 'production') ? 'UA-2468227-6' : 'UA-2468227-5',
   router,
